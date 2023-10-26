8d9bce8a82e48ee0f2f956e09246d21b492fe58e
Mrutyunjaya-Rana <88267791+Mrutyunjaya-Rana@users.noreply.github.com>
2023-10-26 08:07:21 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+//"use strict" // treat all the Js code as newer version
+
+//alert(3 + 3) // not used in node instead used in browser
+
+//console.log(3 + 3) console.log("muku")//semicolon is required to let the compiler know there are two lines
+// instead write like this..
+console.log(3 + 3)
+console.log("muku")
+//code readability should be high
+
+//.........................................
+
+let name = "muku" // string
+let age = 19 // int
+let isLoggedIn = false // boolean
+
+//number => 2 to power 53
+//bigint
+//string => "" 
+//boolean => true/false
+//null => standalone value
+//undefined => when value is not defined
+//symbol => used for uniqueness
+
+//object
+
+console.log(typeof "muku") // string
+console.log(typeof age) // number
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
