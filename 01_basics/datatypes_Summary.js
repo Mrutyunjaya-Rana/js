a0a2b81cc304e60f54ef01a5b8b4300491aa46b6
Mrutyunjaya-Rana <88267791+Mrutyunjaya-Rana@users.noreply.github.com>
2023-10-27 07:49:23 +0000

    memory updated

--- 01_basics/datatypes_Summary.js
+++ 01_basics/datatypes_Summary.js
@@ -1,25 +1,51 @@
-//Broadly datatypes are divided into two types based on how they arey are stored in the memory and how we can access it.
-// 1.PRIMITIVE: (call by value)
-// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
-const id = Symbol('123') 
-const anotherId = Symbol('123')
-console.log(id === anotherId); // false as symbol makes them different
+// //Broadly datatypes are divided into two types based on how they arey are stored in the memory and how we can access it.
+// // 1.PRIMITIVE: (call by value)
+// // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+// const id = Symbol('123') 
+// const anotherId = Symbol('123')
+// console.log(id === anotherId); // false as symbol makes them different
 
-const bigNumber = 2341423525566n 
-console.log(typeof bigNumber); // n at the end makes an int bigInt
+// const bigNumber = 2341423525566n 
+// console.log(typeof bigNumber); // n at the end makes an int bigInt
 
-// 2.NON PRIMITIVE: (call by reference)
-// Array, Objects, Functions
+// // 2.NON PRIMITIVE: (call by reference)
+// // Array, Objects, Functions
 
-const heros = ["shaktiman","spiderman","superman"] // array
+// const heros = ["shaktiman","spiderman","superman"] // array
 
-//object are inside {} and have key value pair
-let muObj = {
-    name: "muku",
-    age: 19,
+// //object are inside {} and have key value pair
+// let muObj = {
+//     name: "muku",
+//     age: 19,
+// }
+
+// //function
+// const myFunction = function(){
+//     console.log("hello world");
+// }
+
+// +++++++++++++ MEMORY ++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+// when Stack memory is used, u get a copy of the variables used.
+// when Heap memory is used, u get a reference of the variables used. Hence what u will change, it will change the original value
+
+//primitive example
+let myName = "muku"
+let anotherName = myName
+anotherName = "mukul"
+console.log(myName);  // muku
+console.log(anotherName); //mukul
+
+//non primitive example
+let userOne = {
+    email : "muku@gmail.com",
+    upi: "user@ybl"
 }
 
-//function
-const myFunction = function(){
-    console.log("hello world");
-}
+let userTwo = userOne
+
+userTwo.email = "mukul@gmail.com"
+
+console.log(userOne.email); //mukul@gmail.com
+console.log(userTwo.email); //mukul@gmail.com
