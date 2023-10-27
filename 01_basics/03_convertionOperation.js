182c200ddca72d0b0dcf0ece78746bba08ce9e9f
Mrutyunjaya-Rana <88267791+Mrutyunjaya-Rana@users.noreply.github.com>
2023-10-27 06:46:12 +0000

    convertion and operation

--- 01_basics/03_convertionOperation.js
+++ 01_basics/03_convertionOperation.js
@@ -1,12 +1,12 @@
 let score = null 
 
-console.log(typeof score); //number when score = 66
+//console.log(typeof score); //number when score = 66
         //OR
 //console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber); //NaN(not a number) when score = "66abc"
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber); //NaN(not a number) when score = "66abc"
 
 // "66" => 66
 // "66abc" => NaN
@@ -15,7 +15,7 @@ console.log(valueInNumber); //NaN(not a number) when score = "66abc"
 let isLoggedIn = "muku"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true
 // "" => false
@@ -24,5 +24,27 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 66
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//*************** OPERATIONS ****************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 ="muku"
+let str3 = str1 + " " +str2
+//console.log(str3);
+
+// console.log( "1"+2); // 12
+// console.log(1 + "2" ); // 12
+// console.log( "1"+2+2); // 122
+// console.log(1+ 2+"2" ); // 32
+
+console.log(true);//true
+console.log(+true);//1
+//console.log(true+); //ERROR..Unexpected token ')'
+console.log(+"");//0
+
