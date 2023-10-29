94804d4b491c87f897099084dac6e5215b05a7b5
Mrutyunjaya-Rana <88267791+Mrutyunjaya-Rana@users.noreply.github.com>
2023-10-29 05:58:40 +0000

    strings started

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,8 @@
+const name = "muku"
+const repoCount = 1
+
+//console.log(name + repoCount + " value");
+ 
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // prefer using this instead of the above one
+
+const gameName = new String("muku-mr-com")
